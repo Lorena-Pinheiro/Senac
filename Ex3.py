d37e9293ec826395c4c4b228f2c5da8a36c2a0a0
Lorena.L.P <107314914+Lorena-Pinheiro@users.noreply.github.com>
2024-05-27 10:15:11 -0300

    1

---
+++ Ex3.py
@@ -0,0 +1,7 @@
+while True:
+    nome=input("Digite seu nome de usuário: ")
+    senha=input("Digite sua senha: ")
+    if(nome==senha):
+        print("A senha não pode ser igual ao nome de usuário")
+    else:
+        break
